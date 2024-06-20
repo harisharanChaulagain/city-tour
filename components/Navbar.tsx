67f892a32ed29d5af4ce8393c822d38868507f36
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2024-06-20 11:08:27 +0545

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,30 +13,36 @@ export default function Navbar() {
   };
 
   useEffect(() => {
+    const body = document.body;
     if (showNavItem) {
       gsap.to(navSectionRef.current, {
         height: "100vh",
         opacity: 1,
         duration: 0.5,
         ease: "power3.inOut",
-        pointerEvents: "auto", 
-        transformOrigin:"top"
+        pointerEvents: "auto",
+        transformOrigin: "top",
       });
+      body.style.overflow = "hidden"; // Disable scrolling
     } else {
       gsap.to(navSectionRef.current, {
         height: 0,
         opacity: 0,
         duration: 0.5,
         ease: "power3.inOut",
-        pointerEvents: "none", 
+        pointerEvents: "none",
       });
+      body.style.overflow = ""; // Enable scrolling
     }
+    return () => {
+      body.style.overflow = ""; // Clean up on unmount
+    };
   }, [showNavItem]);
 
   return (
     <>
-      <div className="bg-gray-200 py-10 ">
-        <header className="w-full flex justify-between items-center bg-gray-200 h-20 px-4 md:px-8 relative">
+      <div className="bg-[#E9E8E4] w-screen">
+        <header className="w-full flex justify-between items-center bg-[#E9E8E4] h-20 px-4 md:px-8 relative">
           <div className="text-xl font-bold">nomad.</div>
           <div onClick={toggleNavItem}>
             {showNavItem ? (
@@ -54,28 +60,38 @@ export default function Navbar() {
           {/* Dropdown menu */}
           <section
             ref={navSectionRef}
-            className="absolute top-20 left-0 bg-white w-full h-0 overflow-hidden z-50"
+            className="absolute top-20 left-0 bg-[#E9E8E4] w-full h-0 overflow-hidden z-50"
             style={{
               opacity: 0,
               transition: "height 0.5s, width 0.5s, opacity 0.5s",
             }}
           >
-            <div className="flex flex-col gap-4 p-4">
-              {navItems.map((item, index) => (
-                <Link
-                  key={index}
-                  href={item?.href}
-                  className="text-2xl font-bold text-[#1D1D1B] hover:text-blue-600 transition-colors"
-                >
-                  {item?.title}
-                </Link>
-              ))}
+            <div className="flex flex-col items-center gap-20 ">
+              <div className="flex flex-col gap-4 p-4 justify-center items-center">
+                {navItems.map((item, index) => (
+                  <Link
+                    key={index}
+                    href={item?.href}
+                    onClick={toggleNavItem}
+                    className="nav-link text-7xl font-bold text-[#1D1D1B] flex items-center"
+                  >
+                    <div className="arrow-wrapper">
+                      <Icon
+                        icon="ooui:arrow-next-ltr"
+                        className="arrow-icon text-7xl"
+                      />
+                    </div>
+                    {item?.title}
+                  </Link>
+                ))}
+              </div>
+
               <div className="flex justify-center items-center gap-8">
                 {socialIcon.map((item, index) => (
                   <Link
                     href={item?.link}
                     key={index}
-                    className="border border-gray-500 rounded-full h-12 w-12 justify-center items-center flex hover:border-[#1D1D1B] transition-all duration-300"
+                    className="border border-gray-400 rounded-full h-12 w-12 justify-center items-center flex hover:border-[#1D1D1B] transition-all duration-300"
                   >
                     {item?.title}
                   </Link>
@@ -100,7 +116,7 @@ const navItems = [
   },
   {
     title: "Faqs",
-    href: "/faqs",
+    href: "/faq",
   },
   {
     title: "Contact",
